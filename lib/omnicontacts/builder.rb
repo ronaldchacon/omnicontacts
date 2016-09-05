c8edd69d6e91dbfe8fa2523ef8afc9894a21c9b7
Ronald Chacon <ronald.chacon87@gmail.com>
2016-09-05 10:37:06 -0700

    Fix issue with rack2

--- lib/omnicontacts/builder.rb
+++ lib/omnicontacts/builder.rb
@@ -3,27 +3,28 @@ require "omnicontacts"
 module OmniContacts
   class Builder < Rack::Builder
     def initialize(app, &block)
-      if rack14?
-        super
-      else
+      if rack13?
         @app = app
         super(&block)
+      else
+        super
       end
     end
 
-    def rack14?
-      Rack.release.split('.')[1].to_i >= 4
+    def rack13?
+      major, minor = Rack.release.split(".").first(2)
+      major.to_i == 1 && minor.to_i <= 3
     end
 
-    def importer importer, *args
+    def importer(importer, *args)
       middleware = OmniContacts::Importer.const_get(importer.to_s.capitalize)
       use middleware, *args
     rescue NameError
       raise LoadError, "Could not find importer #{importer}."
     end
 
-    def call env
-      @ins << @app unless rack14? || @ins.include?(@app)
+    def call(env)
+      @ins << @app if rack13? && !@ins.include?(@app)
       to_app.call(env)
     end
   end
